c918b69fe11c694ffbe769dd85471f4f6f0afcb5
Karla Cruz <karla.cruz@qacg.com>
2016-07-09 10:27:01 -0500

    Se agrega segunda librería de StringUtils, así como modificación a la clase main

--- src/com/qacg/Main.java
+++ src/com/qacg/Main.java
@@ -1,11 +1,14 @@
 package com.qacg;
 
 import com.qacg.utils.StringUtils;
+import com.qacg.utils.StringUtilsSeg;
 
 public class Main {
 	public static void main(String args[]){
 		String s = "HOLA";
-		System.out.println(StringUtils.toLowerCase(s));
+		System.out.println("Minúsculas: "+StringUtils.toLowerCase(s));
+		String s2 = "hola";
+		System.out.println("Mayúsculas: "+StringUtilsSeg.toUpperCase(s2));
 	}
 
 }
---
+++ src/com/qacg/utils/StringUtilsSeg.java
@@ -0,0 +1,11 @@
+package com.qacg.utils;
+
+public class StringUtilsSeg {
+	public static String toUpperCase(String s){
+		if(s != null){
+			return s.toUpperCase();
+		}else{
+			return "";
+		}
+	}
+}
