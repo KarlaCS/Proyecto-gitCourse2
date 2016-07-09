d71cae73467e957766076344315d7bd70e410010
Karla Cruz <karla.cruz@qacg.com>
2016-07-09 10:12:22 -0500

    Subiendo los primeros archivos

---
+++ src/com/qacg/Main.java
@@ -0,0 +1,11 @@
+package com.qacg;
+
+import com.qacg.utils.StringUtils;
+
+public class Main {
+	public static void main(String args[]){
+		String s = "HOLA";
+		System.out.println(StringUtils.toLowerCase(s));
+	}
+
+}
---
+++ src/com/qacg/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.qacg.utils;
+
+public class StringUtils {
+	public static String toLowerCase(String s){
+		if(s != null){
+			return s.toLowerCase();
+		}else{
+			return "";
+		}
+	}
+}
